ae7807ba9a2f860a642e399e068df0729077acba
darrenwong916 <darrenwong06@gmail.com>
2014-01-07 10:49:11 -0800

    Corrected the coding problem

--- Reverse.A.String.rb
+++ Reverse.A.String.rb
@@ -1,16 +1,22 @@
-puts "Enter a string"
+=begin 
+ puts "Enter a string"
 
 phrase = gets.chomp
 rev_phrase = ""
 for i in (0)phrase.length
  rev_phrase(i) = rev_phrase[-i-1]
 puts rev_phrase
-end
+end  
+=end
+ 
 
-
-until i == len/2
+puts "enter a phrase"
+string = gets.chomp 
+i = 0
+until i == string.length/2
 	temp = string[i]
 	string[i] = string[-i-1]
 	string[-i-1] = temp
 	i += 1
 end
+puts string
