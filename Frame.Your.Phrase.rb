b3a5f808f3c8ad029e1be3a3569222ccb23d2216
darrenwong916 <darrenwong06@gmail.com>
2014-01-07 11:54:36 -0800

    Remodified

--- Frame.Your.Phrase.rb
+++ Frame.Your.Phrase.rb
@@ -1,6 +1,14 @@
 puts "Please enter a phrase to be framed"
-phrase = gets.chomp
-phrase.max = x
-x.length = length
-tbstar = ""
+phrase = gets.chomp.split
+arr = phrase.map {|x| x.length}
+arr_max = arr.max {|x,y| x <=>}
+puts "**" + " "*arr_max + "*"
+phrase.each do |x|
+	if x.length < arr_max
+		puts "* " + x + " "*(arr_max-x.length) + " *"
+	else puts "* " + x + " *"
+	end
+end
+
+puts "**"+"*"*arr_max + "**"
 
